c7f4213cf31b5dd6e05b1cc7863213463b653411
mosa <U>
2022-02-17 17:37:02 +0330

    a few changes

--- fekrebekre.py
+++ fekrebekre.py
@@ -8,45 +8,62 @@ PG = ''
 def Passmaker():
     num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     for key in lockpass:
-        lockpass[key] = random.choice(num)
+        lockpass[key] = str(random.choice(num))
+    return lockpass
 
 
-def Playergussdecleaer(PG):
+def Playergussdecleaer():
+    PG = input('Enter Your Guss : ')
     PG = list(PG.strip())
     # fixing error with 'reversed()'->Index Out of Range (for revoming item)
     for item in reversed(range(len(PG))):
         if not PG[item].isnumeric():
             PG.remove(PG[item])
     if len(PG) < 4:
-        print('your guss is to short')
+        print('your guss is to short , try another')
+        return Playergussdecleaer()
     elif len(PG) > 4:
-        print('your guss is to long')
+        print('your guss is to long , try another')
+        return Playergussdecleaer()
+
     else:
-        return PG
+        i = 0
+        for key in playerguss:
+            playerguss[key] = PG[i]
+            i += 1
+        return playerguss
 
 
-def Comparator():
+def Comparator(playerguss, lockpass):
     RNRP = 0  # right num in right place
     RNWP = 0  # right num in wrong place
     for key in playerguss:
         if playerguss[key] == lockpass[key]:
             RNRP += 1
-            continue
 
-        elif playerguss[key] in lockpass:
+        elif playerguss[key] in lockpass.values():
             RNWP += 1
-            continue
 
-        else:
-            pass
-    if RNRP > 0:
-        print('{} right num in right place'.format(RNRP), end=" ")
-    elif RNRP > 0 and RNWP > 0:
-        print('and', end=" ")
-    elif RNWP > 0:
+    if RNRP > 0 and RNRP < 4:
+        print('{} right num in right place'.format(RNRP))
+    if RNRP > 0 and RNWP > 0:
+        print('and')
+    if RNWP > 0 and RNWP < 4:
         print('{} right num in wrong place'.format(RNWP))
+    if RNRP == 4:
+        lock = False
+        print('You Win')
+        return lock
 
 
-Passmaker()
-PG = input()
-Playergussdecleaer(PG)
+going = True
+lock = True
+while going:
+    lockpass = Passmaker()
+    print(lockpass)
+    while lock:
+        playerguss = Playergussdecleaer()
+        action = Comparator(playerguss, lockpass)
+        if action == False:
+            lock = False
+    going = False
